4838f3dadfb10e99f04141f0143fc047d273263f
Bryan Chavarría Hughes <bryannsi@outlook.com>
2024-08-28 21:32:24 -0600

    New method to get a record of medical history

--- src/medical/medical.route.js
+++ src/medical/medical.route.js
@@ -5,6 +5,7 @@ export function createMedicalRouter () {
   const medicalRouter = Router()
   // The end points start with= /pet
   medicalRouter.get('/:id/medical-history', MedicalController.getMedicalHistory)
+  medicalRouter.get('/:id/medical-history/:recordId', MedicalController.getMedicalRecord)
   medicalRouter.post('/:id/medical-history', MedicalController.addMedicalRecord)
   medicalRouter.put('/:id/medical-history/:recordId', MedicalController.updateMedicalRecord)
   medicalRouter.delete('/:id/medical-history/:recordId', MedicalController.deleteMedicalRecord)

4a8c2d13d66a20c74b81f138a2c29496753b5dfa
Bryan Chavarría Hughes <bryannsi@outlook.com>
2024-09-12 13:28:31 -0600

    Add .env and export env in config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,11 @@
-export const {
-  PORT = 3000
-} = process.env
+export const PORT = process.env.PORT || 3000
+
+export const DB = {
+  host: process.env.DB_HOST || null,
+  port: process.env.DB_PORT || null,
+  database: process.env.DB_DATABASE || null,
+  user: process.env.DB_USER || null,
+  password: process.env.DB_PASSWORD || null,
+  max: process.env.DB_MAX ? parseInt(process.env.DB_MAX, 10) : 10,
+  sslRejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true'
+}

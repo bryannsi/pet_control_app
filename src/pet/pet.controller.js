abad164883e9ed60a4a8cab82cb37e51c895a6d9
Bryan Chavarría Hughes <bryannsi@outlook.com>
2024-09-13 00:57:29 -0600

    set pet controller methods to async, implement pet.model

--- src/pet/pet.controller.js
+++ src/pet/pet.controller.js
@@ -1,29 +1,32 @@
+import { pet } from './pet.model.js'
 export class PetController {
-  static createPet (req, res) {
-    console.log('Mascota creada')
+  static async createPet (req, res) {
+    const result = pet.create(req.body)
+    console.log(result)
     res.send('this is createPet method')
     // Lógica para crear una mascota
   }
 
-  static modifyPet (req, res) {
-    const { id } = req.params
-    console.log(`Mascota con ID ${id} modificada`)
+  static async modifyPet (req, res) {
+    const data = { id: req.params.id, ...req.body }
+    const result = pet.modify(data)
+    console.log(result)
     res.send('this is modifyPet method')
     // Lógica para modificar una mascota según su ID
   }
 
-  static deletePet (req, res) {
+  static async deletePet (req, res) {
     const { id } = req.params
     console.log(`Mascota con ID ${id} eliminada`)
     res.send('this is deletePet method')
     // Lógica para eliminar una mascota según su ID
   }
 
-  static getPets (req, res) {
+  static async getPets (req, res) {
     res.send('this is getPets method')
   }
 
-  static findPetById (req, res) {
+  static async findPetById (req, res) {
     const { id } = req.params
     console.log(`La mascota con el ID es: ${id}`)
     res.send('this is findPetById method')

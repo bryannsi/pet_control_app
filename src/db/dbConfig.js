3ece035c5f8164161447770fabf2134abafc5127
Bryan Chavarría Hughes <bryannsi@outlook.com>
2024-09-13 00:57:46 -0600

    Add dbConfig

---
+++ src/db/dbConfig.js
@@ -0,0 +1,24 @@
+// db.js
+import { DB } from '#utils/config.js' // Importa las variables de entorno desde config.js
+import pgPromise from 'pg-promise'
+
+// Database configuration object
+const dbConfig = {
+  host: DB.host,
+  port: DB.port,
+  database: DB.database,
+  user: DB.user,
+  password: DB.password,
+  max: DB.max,
+  ssl: { rejectUnauthorized: DB.sslRejectUnauthorized } // SSL settings for secure connections
+}
+
+// Initialize pg-promise
+
+// Initialize the database connection
+const db = pgPromise()(dbConfig)
+
+console.log('Database connection pool initialized.')
+
+// Export the database object for use in other files
+export { db }

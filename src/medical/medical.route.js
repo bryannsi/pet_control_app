bdf9fa25f584a5fd185ef7a40c252f134ff42e71
Bryan Chavarría Hughes <bryannsi@outlook.com>
2024-09-15 14:09:58 -0600

    Fixed medical route error with params

--- src/medical/medical.route.js
+++ src/medical/medical.route.js
@@ -1,14 +1,14 @@
 import { Router } from 'express'
-import { MedicalController } from '../medical/medical.controller.js'
+import { MedicalController } from './medical.controller.js'
 
 export function createMedicalRouter () {
   const medicalRouter = Router()
   // The end points start with= /pet
-  medicalRouter.get('/petId/medical-history', MedicalController.getMedicalHistory)
-  medicalRouter.get('/petId/medical-history/:recordId', MedicalController.getMedicalRecord)
-  medicalRouter.post('/petId/medical-history', MedicalController.addMedicalRecord)
-  medicalRouter.put('/petId/medical-history/:recordId', MedicalController.updateMedicalRecord)
-  medicalRouter.delete('/petId/medical-history/:recordId', MedicalController.deleteMedicalRecord)
+  medicalRouter.get('/:petId/medical-history', MedicalController.getMedicalHistory) // medical-history?limit=intValue&offset=intValue
+  medicalRouter.get('/:petId/medical-history/:recordId', MedicalController.getMedicalRecord)
+  medicalRouter.post('/:petId/medical-history', MedicalController.addMedicalRecord)
+  medicalRouter.put('/:petId/medical-history/:recordId', MedicalController.updateMedicalRecord)
+  medicalRouter.delete('/:petId/medical-history/:recordId', MedicalController.deleteMedicalRecord)
 
   return medicalRouter
 }

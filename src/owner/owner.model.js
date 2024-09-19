36acb8c4b52d4aabf99a508d09adb52b07ba0078
Bryan Chavarría Hughes <bryannsi@outlook.com>
2024-09-18 23:03:36 -0600

    Change handleQuery path

--- src/owner/owner.model.js
+++ src/owner/owner.model.js
@@ -1,5 +1,5 @@
 import { db } from '../db/dbConfig.js'
-import handleQuery from '../db/utils/handleQuery.js'
+import handleQuery from '../db/handleQuery.js'
 
 const getAlls = async (limit, offset) => {
   return handleQuery(

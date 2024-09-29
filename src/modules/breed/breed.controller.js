231b8d47de76a8fd092fc302b7ceda4856942db2
Bryan Chavarría Hughes <bryannsi@outlook.com>
2024-09-28 19:21:57 -0600

    Extends BaseController to BreedController

--- src/modules/breed/breed.controller.js
+++ src/modules/breed/breed.controller.js
@@ -1,65 +1,48 @@
+import BaseController from '#modules/baseController.js'
 import { breed } from './breed.model.js'
 
-export class BreedController {
-  static async getAlls (req, res) {
-    try {
+export class BreedController extends BaseController {
+  static async getAlls (req, res, next) {
+    BaseController.handleRequest(req, res, next, async () => {
       let { limit, offset } = req.query
 
       limit = parseInt(limit, 10)
       offset = parseInt(offset, 10)
 
-      const result = await breed.getAlls(limit, offset)
-      res.status(200).json(result)
-    } catch (error) {
-      console.error('Error getting the breeds:', error)
-      res.status(500).json({ error: 'Failed to retrieve breeds' })
-    }
+      const data = await breed.getAlls(limit, offset)
+      return data && data.length > 0 ? { statusCode: 200, data } : { statusCode: 404, data: null }
+    })
   }
 
-  static async findById (req, res) {
-    try {
+  static async findById (req, res, next) {
+    BaseController.handleRequest(req, res, next, async () => {
       const { breedId } = req.params
-      const result = await breed.findById(breedId)
-      if (result && result.length > 0) {
-        res.status(200).json(result)
-      } else {
-        res.status(404).json({ error: 'Record not found' })
-      }
-    } catch (error) {
-      // console.error('Error getting the breed:', error)
-      res.status(500).json({ error: 'Failed to retrieve the breed' })
-    }
+      const data = await breed.findById(breedId)
+      return data && data.length > 0 ? { statusCode: 200, data } : { statusCode: 404, data: null }
+    })
   }
 
-  static async create (req, res) {
-    try {
+  static async create (req, res, next) {
+    BaseController.handleRequest(req, res, next, async () => {
       const breedData = req.body
-      const result = await breed.create(breedData)
-      res.status(201).json({ message: 'successfully created', data: { id: result } })
-    } catch (error) {
-      console.error('Error adding breed record:', error)
-      res.status(500).json({ error: 'Failed to add breed record' })
-    }
+      const data = await breed.create(breedData)
+      return { statusCode: 201, data }
+    })
   }
 
-  static async modify (req, res) {
-    try {
+  static async modify (req, res, next) {
+    BaseController.handleRequest(req, res, next, async () => {
       const breedData = { breedId: req.params.breedId, ...req.body }
-      const result = await breed.modify(breedData)
-      result ? res.status(200).json({ message: 'successfully updated' }) : res.status(404).json({ error: 'Record not found' })
-    } catch (error) {
-      console.error('Error updating breed record:', error)
-    }
+      const data = await breed.modify(breedData)
+      return { statusCode: 200, data }
+    })
   }
 
-  static async remove (req, res) {
-    try {
+  static async remove (req, res, next) {
+    BaseController.handleRequest(req, res, next, async () => {
       const { breedId } = req.params
-      const result = await breed.remove(breedId)
-      result ? res.status(200).json({ message: 'successfully removed' }) : res.status(404).json({ error: 'Record not found' })
-    } catch (error) {
-      console.error('Error removing breed record:', error)
-      res.status(500).json({ error: 'Failed to remove breed record' })
-    }
+      const data = await breed.remove(breedId)
+      return { statusCode: 200, data }
+    })
   }
 }
